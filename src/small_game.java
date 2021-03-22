aef72f10ab9b83d374ba0ea2fd24345c0c14b7bd
Thi Bich Loan Chung <bichloan.1032170@gmail.com>
2021-03-22 14:46:02 +0100

    count the number of occurrences of 0,1,2

--- src/small_game.java
+++ src/small_game.java
@@ -12,7 +12,22 @@ public class small_game {
 			arrOfInt.add(x);
 		}
 		System.out.println(arrOfInt);
-		System.out.println(arrOfInt.size());
+		int num0 = 0;
+		int num1 = 0;
+		int num2 = 0;
+		for (int y = 0; y < arrOfInt.size(); y++) {
+			if (arrOfInt.get(y) == 0) {
+				num0 = num0 + 1;
+			}else if (arrOfInt.get(y) == 1) {
+				num1 = num1 + 1;
+			}else if (arrOfInt.get(y) == 2) {
+				num2 = num2 + 1;
+			}
+			
+		}
+		System.out.println(num0);
+		System.out.println(num1);
+		System.out.println(num2);
 		
 	}
 
